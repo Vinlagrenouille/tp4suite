cefa7c2676941d6c6ee92c7143357ad04dc4f80b
groc.incognito <groc.incognito@gmail.com>
2016-05-18 04:16:07 -0400

    fin ex1 et dbut ex2

--- ex1.php
+++ ex1.php
@@ -9,60 +9,61 @@
 
 <body>
   <h1>
-    Films
+    Ajout
   </h1>
-  <FORM method='get' action='filmexo2.php'>
-    <SELECT name='real'>
-      <?php
-      $link = mysqli_connect("http://dwarves.iut-fbleau.fr","navales","JwLy54NVALVQDRAn","navales");
+  <FORM method='get' action='ex1.php'>
+      <p>
+        Prenom<br>
+        <input type="text" name="prenom"/><br>Nom<br>
+        <input type="text" name="nom"/><br>Annee<br>
+        <input type="text" name="annee"/><br>
+        <input type="submit" value="Valider" />
+      </p>
+    <?php
+      extract($_GET);
+      if(isset($_GET['nom'])){
+        $nom = $_GET['nom'];
+      }
+      if(isset($_GET['prenom'])){
+        $prenom = $_GET['prenom'];
+      }
+      if(isset($_GET['annee'])){
+        $annee = $_GET['annee'];
+      }
+      $link = mysqli_connect("dwarves.iut-fbleau.fr","navales","JwLy54NVALVQDRAn","navales");
       if(!$link){
         die("<p>Connexion au serveur impossible</p>");
       }
-      $res = mysqli_query($link,"select distinct nom from Artiste, Film where idMes = idArtiste");
-      echo "<option value=\"tous\" > Tous ";
-      foreach($res as $value){
-        echo "<option value=".$value['nom'].">".$value['nom'];
+      if(isset($nom) && isset($prenom) && isset($annee)){
+        mysqli_query($link,"insert into Artiste(nom,prenom,anneeNaiss) values (\"$nom\",\"$prenom\",\"$annee\")");
       }
-      ?>
-      <INPUT TYPE="submit" ></INPUT>
-    </SELECT>
+      mysqli_close($link);
+    ?>
   </FORM>
+  <h1>
+    Films
+  </h1>
   <table>
     <thead>
       <tr>
-        <th>Titre</th>
+        <th>Artiste</th>
         <th>Annee</th>
-        <th>Genre</th>
-        <th>Real</th>
       </tr>
     </thead>
     <?php
-        if(isset($_GET['real'])){
-          $real=$_GET['real'];
-        }
-        else if(empty($_GET['real'])){
-          $real = "tous";
-        }
-      $link = mysqli_connect("http://dwarves.iut-fbleau.fr","navales","JwLy54NVALVQDRAn","navales");
+      $link = mysqli_connect("dwarves.iut-fbleau.fr","navales","JwLy54NVALVQDRAn","navales");
       if(!$link){
         die("<p>Connexion au serveur impossible</p>");
       }
-      if($real == "tous"){
-        $res = mysqli_query($link,"select f.titre,f.annee,f.genre,a.nom from Film f, Artiste a where f.idMes = a.idArtiste");
-      }
-      else{
-        $res = mysqli_query($link,"select f.titre,f.annee,f.genre,a.nom from Film f, Artiste a where f.idMes = a.idArtiste and nom =\"$real\"");
-      }
+      $res = mysqli_query($link,"select nom, prenom, anneeNaiss from Artiste");
       if(!$res){
         die("<p>Resultat non-disponible</p>");
       }
       else{
         foreach($res as $value){
           echo "<tr>";
-          echo "<td>".$value['titre']."</td>";
-          echo "<td>".$value['annee']."</td>";
-          echo "<td>".$value['genre']."</td>";
-          echo "<td>".$value['nom']."</td>";
+          echo "<td>".$value['nom']." ".$value['prenom']."</td>";
+          echo "<td>".$value['anneeNaiss']."</td>";
           echo "</tr>";
         }
        }
